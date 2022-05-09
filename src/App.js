3c95b842e21d0cb0e995c2f1140615935dc944e9
kimdaeyoon <eodbszla@gmail.com>
2022-05-09 23:06:06 +0900

    Component preload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, Suspense, lazy } from "react";
+import React, { useState, useEffect, Suspense, lazy } from "react";
 import styled from "styled-components";
 import Header from "./components/Header";
 import InfoTable from "./components/InfoTable";
@@ -11,6 +11,16 @@ const LazyImageModal = lazy(() => import("./components/ImageModal"));
 function App() {
   const [showModal, setShowModal] = useState(false);
 
+  //방법1. 파일이 클 경우는 모든 파일이 다운로드되고 나서 다운로드 될 수 있도록 useEffect를 활용
+  useEffect(() => {
+    const component = import("./components/ImageModal");
+  }, []);
+
+  //방법2. 마우스가 버튼 위에 올라왔을때 이미지모달을 불러오는 함수
+  // const handleMouseEnter = () => {
+  //   const component = import("./components/ImageModal");
+  // };
+
   return (
     <div className="App">
       <Header />
@@ -19,6 +29,8 @@ function App() {
         onClick={() => {
           setShowModal(true);
         }}
+        //마우스가 위에 올라 왔을떄 실행되는 이벤트
+        /* onMouseEnter={handleMouseEnter} */
       >
         올림픽 사진 보기
       </ButtonModal>
